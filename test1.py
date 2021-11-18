57a26d227f80a38046313cb6dcb90a8421bbd52f
Daniil <92990950+DaniilYarchuk@users.noreply.github.com>
2021-11-18 10:05:10 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,30 @@
+import re
+
+result=[]
+pattern = r"hostname \S*"
+i=1
+k=0
+l=1
+while 1:
+    try:
+        file = open("S%d.txt" %i,"r")
+    
+    except FileNotFoundError :
+        while k < len(result):
+            while l < len(result):
+                if result[l] == result[k]:
+                    print('Error. Find swithes with identical hostnames.')
+                    exit(1)
+                l=l+1
+            k=k+1
+            l=k+1
+        if len(result)==0:
+            print('Error. Dont find files with configuration of switches.')
+        print('Test is successfull!')
+        exit(0)
+
+
+    match=re.search(pattern, file.read())
+    result.append(match[0])
+    file.close()
+    i=i+1
